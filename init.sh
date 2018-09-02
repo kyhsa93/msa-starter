963a637dafe4c3af0aad638cf2f28d9eff3f8de1
Young hoon Go <kyhsa93@naver.com>
2018-09-02 19:33:24 +0900

    first commit

---
+++ init.sh
@@ -0,0 +1,11 @@
+git config --global credential.helper cache
+
+git clone https://github.com/kyhsa93/express-api-starter.git express-api
+
+cd express-api && npm install @babel/core @babel/cli && npm run trans && cd ..
+
+sudo docker-compose up -d
+
+sudo docker images
+
+sudo docker ps -a
